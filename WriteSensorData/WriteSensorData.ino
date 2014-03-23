2341c29dfe428525142a40140e73056f2b802d7a
William Woodruff <william@tuffbizz.com>
2014-03-22 20:36:21 -0400

    fixed bugs

--- WriteSensorData/WriteSensorData.ino
+++ WriteSensorData/WriteSensorData.ino
@@ -6,7 +6,6 @@
 #define PACKETLEN (NUMCHANNELS * 2 + HEADERLEN + 1)
 #define SAMPFREQ 256                      
 #define TIMER2VAL (1024/(SAMPFREQ))                          
-#define LED1  13
 #define CAL_SIG 9
 
 // struct packet
@@ -43,23 +42,23 @@ void setup(void)
 
 	pinMode(CAL_SIG, OUTPUT);
 
-	TXBuf[0] = 0xa5;    //Sync 0
-	TXBuf[1] = 0x5a;    //Sync 1
-	TXBuf[2] = 2;       //Protocol version
-	TXBuf[3] = 0;       //Packet counter
-	TXBuf[4] = 0x02;    //CH1 High Byte
-	TXBuf[5] = 0x00;    //CH1 Low Byte
-	TXBuf[6] = 0x02;    //CH2 High Byte
-	TXBuf[7] = 0x00;    //CH2 Low Byte
-	TXBuf[8] = 0x02;    //CH3 High Byte
-	TXBuf[9] = 0x00;    //CH3 Low Byte
-	TXBuf[10] = 0x02;   //CH4 High Byte
-	TXBuf[11] = 0x00;   //CH4 Low Byte
-	TXBuf[12] = 0x02;   //CH5 High Byte
-	TXBuf[13] = 0x00;   //CH5 Low Byte
-	TXBuf[14] = 0x02;   //CH6 High Byte
-	TXBuf[15] = 0x00;   //CH6 Low Byte 
-	TXBuf[2 * NUMCHANNELS + HEADERLEN] =  0x01;
+	packet[0] = 0xa5;    //Sync 0
+	packet[1] = 0x5a;    //Sync 1
+	packet[2] = 2;       //Protocol version
+	packet[3] = 0;       //Packet counter
+	packet[4] = 0x02;    //CH1 High Byte
+	packet[5] = 0x00;    //CH1 Low Byte
+	packet[6] = 0x02;    //CH2 High Byte
+	packet[7] = 0x00;    //CH2 Low Byte
+	packet[8] = 0x02;    //CH3 High Byte
+	packet[9] = 0x00;    //CH3 Low Byte
+	packet[10] = 0x02;   //CH4 High Byte
+	packet[11] = 0x00;   //CH4 Low Byte
+	packet[12] = 0x02;   //CH5 High Byte
+	packet[13] = 0x00;   //CH5 Low Byte
+	packet[14] = 0x02;   //CH6 High Byte
+	packet[15] = 0x00;   //CH6 Low Byte 
+	packet[2 * NUMCHANNELS + HEADERLEN] =  0x01;
 
 	FlexiTimer2::set(TIMER2VAL, Timer2_Overflow_ISR);
 	FlexiTimer2::start();
@@ -75,12 +74,12 @@ void Timer2_Overflow_ISR(void)
 	{
 		adc_value = analogRead(current_channel);
 		packet[((2*current_channel) + HEADERLEN)] = ((unsigned char) ((adc_value & 0xFF00) >> 8));
-		packet[((2*current_channel) + HEADERLEN + 1)] = ((unsigned char) (adc_value & 0xFF00);
+		packet[((2*current_channel) + HEADERLEN + 1)] = ((unsigned char) (adc_value & 0x00FF));
 	}
 
 	for (index = 4; index < 16; index++)
 	{
-		Serial.write(packet[index]);
+		Serial.write(packet[index] & 0x3E);
 	}
 
 	packet[3]++;
@@ -91,4 +90,11 @@ void Timer2_Overflow_ISR(void)
 		counter = 0;
 		toggle_CAL_SIG();
 	}
+}
+
+void loop()
+{
+  
+ __asm__ __volatile__ ("sleep");
+ 
 }
